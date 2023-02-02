5805a674568d39bee45b26970463a4680f25b9ff
Empty <parhambarnamehnevis@gmail.com>
2023-02-03 02:35:44 +0330

    Add files via upload

---
+++ testcloud.py
@@ -0,0 +1,38 @@
+import subprocess
+import os 
+import sys
+
+# color
+color_reset = "\033[0m"
+color_green = "\33[32m"
+color_yellow = "\033[33m"
+color_red = "\33[91m"
+color_bold = "\033[1m"
+
+arg = sys.argv[1]
+
+# main_path=os.path.expanduser("~/CloudFlare")
+# check_path = os.system(f"ls {main_path} &> /dev/null")
+# if check_path != 0:
+#     os.mkdir(main_path)
+# else:
+#     print(f"{color_red}Project PATH Already Exist ... {color_reset}")
+
+
+def find_fast_server():
+    print(f"{color_bold}Testing the Servers ... {color_reset}")
+    list_server = subprocess.check_output(f"cat {arg}",shell=True,encoding='utf-8').replace(" ",'').split()
+
+    avrs = []
+    for i in list_server:
+        if os.system(f"ping -c1 {i} &> /dev/null") == 0:            
+            a = subprocess.check_output(f"ping -c 3 {i} | tail -n1 | cut -d' ' -f4 | cut -d'/' -f2",shell=True,encoding="utf-8").replace("\n" , "")
+            avrs.append(float(a))
+            print(f"{i} {color_green}OK{color_reset}   Speed:{color_yellow} {a} {color_reset}ms")
+        else:
+            print(f"{i} {color_red} Not Working!{color_reset}")
+            list_server.remove(i)
+    print(color_reset)
+
+
+find_fast_server()
